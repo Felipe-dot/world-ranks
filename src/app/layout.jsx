6e185a9d6fb7b53b3229a04d915cd92abe782fd3
Felipe-dot <felipeluis610@gmail.com>
2024-03-25 11:20:40 -0300

    Enable overflow

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,7 +9,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className="bg-[--gray-black] overflow-hidden">
+      <body className="bg-[--gray-black]">
         <header className="bg-[url('/hero-image-wr.jpg')] w-screen h-80 bg-cover bg-center flex justify-center items-center ">
           <Image src="Logo.svg" alt="Logo image" height={250} width={250} />
         </header>
